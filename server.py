a07aac6061b26c21b03934e1fdf7d728004109bf
Adam Derewecki <derewecki@gmail.com>
2011-05-20 03:23:25 -0400

    end to end

--- server.py
+++ server.py
@@ -1,10 +1,17 @@
+import logging
 import sys
 import json
 
+import bottle
 from bottle import route
 from bottle import run
 from bottle import static_file
-from build_histogram import bucket_log
+
+from vendor.scripts import build_histogram
+
+bottle.debug(True)
+log = logging.getLogger('servlet')
+log.setLevel(logging.DEBUG)
 
 @route('/')
 def main_page():
@@ -13,11 +20,16 @@ def main_page():
 @route('/:filename')
 def server_static(filename):
     print filename
+    log.info(filename)
     return static_file(filename, root='static')
 
 @route('/api/data')
 def data():
-	return json.dumps(bucket_log('midtowndoornail.log'))
+    points = build_histogram.bucket_log('/home/derwiki/src/histogram/midtowndoornail-dates.log', format_output=True, bucket_seconds=300)
+    data = dict((d, [count]) for d, count in points)
+    print data.items()[0:10]
+    print json.dumps(data)
+    return json.dumps(data)
 
 port = sys.argv[1] if len(sys.argv) > 1 else 8080
 run(host='0.0.0.0', port=port)
