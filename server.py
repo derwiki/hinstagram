fbfcba9f329b8df5de42a4aa25412b8721b8b9c4
Adam Derewecki <derewecki@gmail.com>
2011-05-20 07:11:25 -0700

    Cleaned up server.py

--- server.py
+++ server.py
@@ -3,6 +3,7 @@ import sys
 import json
 
 import bottle
+from bottle import request
 from bottle import route
 from bottle import run
 from bottle import static_file
@@ -19,16 +20,14 @@ def main_page():
 
 @route('/:filename')
 def server_static(filename):
-    print filename
-    log.info(filename)
+    log.info("Static: %s" % filename)
     return static_file(filename, root='static')
 
 @route('/api/data')
 def data():
-    points = build_histogram.bucket_log('/home/derwiki/src/histogram/midtowndoornail-dates.log', format_output=True, bucket_seconds=300)
+    bucket_seconds = request.GET.get('bucket_seconds', 300)
+    points = build_histogram.bucket_log('midtowndoornail-dates.log', format_output=True, bucket_seconds=bucket_seconds)
     data = dict((d, [count]) for d, count in points)
-    print data.items()[0:10]
-    print json.dumps(data)
     return json.dumps(data)
 
 port = sys.argv[1] if len(sys.argv) > 1 else 8080
